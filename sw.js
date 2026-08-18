f9de89e87d96c814803640930aa29f0a572c32fe
ma2hir0 <ma2ihir0n0ri@gmail.com>
2026-08-18 21:10:12 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("push", e => {
+  e.waitUntil(
+    self.registration.showNotification(
+      "KoBoTrade Pushテスト",
+      { body: "Pushイベントを受信しました。" }
+    )
+  );
+});
